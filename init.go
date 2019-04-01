18e1cc665962d16c81ca1b10fcae17e1c21f114f
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2019-04-01 14:06:35 +0300

    PMM-3719 Add envvar names to the help output.

--- init.go
+++ init.go
@@ -30,9 +30,9 @@ var (
 
 //nolint:gochecknoinits
 func init() {
-	debugF := flag.Bool("pmm.debug", false, "Enable debug output.")
-	traceF := flag.Bool("pmm.trace", false, "Enable trace output.")
-	serverURLF := flag.String("pmm.server-url", "https://127.0.0.1:8443/", "PMM Server URL.")
+	debugF := flag.Bool("pmm.debug", false, "Enable debug output [PMM_DEBUG].")
+	traceF := flag.Bool("pmm.trace", false, "Enable trace output [PMM_TRACE].")
+	serverURLF := flag.String("pmm.server-url", "https://127.0.0.1:8443/", "PMM Server URL [PMM_SERVER_URL].")
 	flag.Parse()
 	envvars := map[string]*flag.Flag{
 		"PMM_DEBUG":      flag.Lookup("pmm.debug"),
