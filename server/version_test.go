c405c0b4d9cd7c5e027eedc8ad796822bd385323
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2019-06-17 13:22:47 +0300

    PMM-4197 Remove version test for now.

--- server/version_test.go
+++ server/version_test.go
@@ -38,10 +38,10 @@ func TestVersion(t *testing.T) {
 
 			assert.True(t, strings.HasPrefix(res.Version, "2.0.0"), "version = %q", res.Version)
 
-			tt := pmmapitests.ExpectFailure(t, "https://jira.percona.com/browse/PMM-4114")
-			defer tt.Check()
-
-			assert.True(tt, strings.HasPrefix(res.Version, "2.0.0-"), "version = %q should has suffix", res.Version)
+			// TODO https://jira.percona.com/browse/PMM-4160
+			// tt := pmmapitests.ExpectFailure(t, "https://jira.percona.com/browse/PMM-4114")
+			// defer tt.Check()
+			// assert.True(tt, strings.HasPrefix(res.Version, "2.0.0-"), "version = %q should has suffix", res.Version)
 		})
 	}
 }
