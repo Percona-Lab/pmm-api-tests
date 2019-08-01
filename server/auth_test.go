d6cb7f5e9cb74cdd61758bfadbe8966c3950793e
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2019-08-01 20:51:36 +0300

    PMM-4425 Tests for authentication errors.

--- server/auth_test.go
+++ server/auth_test.go
@@ -2,36 +2,73 @@ package server
 
 import (
 	"bytes"
+	"fmt"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"testing"
 
+	"github.com/AlekSi/pointer"
+	serverClient "github.com/percona/pmm/api/serverpb/json/client"
+	"github.com/percona/pmm/api/serverpb/json/client/server"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
 
 	pmmapitests "github.com/Percona-Lab/pmm-api-tests"
 )
 
 func TestAuth(t *testing.T) {
-	t.Parallel()
+	t.Run("AuthErrors", func(t *testing.T) {
+		for user, httpCode := range map[*url.Userinfo]int{
+			nil:                              401,
+			url.UserPassword("bad", "wrong"): 401,
+			// TODO test 403
+		} {
+			user := user
+			httpCode := httpCode
+			t.Run(fmt.Sprintf("%s/%d", user, httpCode), func(t *testing.T) {
+				t.Parallel()
 
-	// make a BaseURL without authentication
-	baseURL, err := url.Parse(pmmapitests.BaseURL.String())
-	require.NoError(t, err)
-	baseURL.User = nil
+				// copy BaseURL and replace auth
+				baseURL, err := url.Parse(pmmapitests.BaseURL.String())
+				require.NoError(t, err)
+				baseURL.User = user
 
-	uri := baseURL.ResolveReference(&url.URL{
-		Path: "v1/version",
+				uri := baseURL.ResolveReference(&url.URL{
+					Path: "v1/version",
+				})
+
+				t.Logf("URI: %s", uri)
+				resp, err := http.Get(uri.String())
+				require.NoError(t, err)
+				defer resp.Body.Close() //nolint:errcheck
+
+				b, err := httputil.DumpResponse(resp, true)
+				require.NoError(t, err)
+				assert.Equal(t, httpCode, resp.StatusCode, "response:\n%s", b)
+				require.False(t, bytes.Contains(b, []byte(`<html>`)), "response:\n%s", b)
+			})
+		}
 	})
 
-	t.Logf("URI: %s", uri)
-	resp, err := http.Get(uri.String())
-	require.NoError(t, err)
-	defer resp.Body.Close() //nolint:errcheck
-	b, err := httputil.DumpResponse(resp, true)
-	require.NoError(t, err)
+	t.Run("NormalErrors", func(t *testing.T) {
+		for grpcCode, httpCode := range map[codes.Code]int{
+			codes.Unauthenticated:  401,
+			codes.PermissionDenied: 403,
+		} {
+			grpcCode := grpcCode
+			httpCode := httpCode
+			t.Run(fmt.Sprintf("%s/%d", grpcCode, httpCode), func(t *testing.T) {
+				t.Parallel()
 
-	assert.Equal(t, 401, resp.StatusCode, "response:\n%s", b)
-	assert.False(t, bytes.Contains(b, []byte(`<html>`)), "response:\n%s", b)
+				res, err := serverClient.Default.Server.Version(&server.VersionParams{
+					Dummy:   pointer.ToString(fmt.Sprintf("grpccode-%d", grpcCode)),
+					Context: pmmapitests.Context,
+				})
+				assert.Empty(t, res)
+				pmmapitests.AssertAPIErrorf(t, err, httpCode, grpcCode, "gRPC code %d (%s)", grpcCode, grpcCode)
+			})
+		}
+	})
 }
