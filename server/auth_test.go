0ac0f74ed46e82575cc68a0dd1661e316cdf61a3
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2020-01-20 15:02:19 +0300

    PMM-5137 Add tests for Swagger URLs.

--- server/auth_test.go
+++ server/auth_test.go
@@ -21,15 +21,27 @@ import (
 	pmmapitests "github.com/Percona-Lab/pmm-api-tests"
 )
 
+func doRequest(t testing.TB, client *http.Client, req *http.Request) (*http.Response, []byte) {
+	resp, err := client.Do(req)
+	require.NoError(t, err)
+
+	defer resp.Body.Close() //nolint:errcheck
+
+	b, err := ioutil.ReadAll(resp.Body)
+	require.NoError(t, err)
+
+	return resp, b
+}
+
 func TestAuth(t *testing.T) {
 	t.Run("AuthErrors", func(t *testing.T) {
-		for user, httpCode := range map[*url.Userinfo]int{
+		for user, code := range map[*url.Userinfo]int{
 			nil:                              401,
 			url.UserPassword("bad", "wrong"): 401,
 		} {
 			user := user
-			httpCode := httpCode
-			t.Run(fmt.Sprintf("%s/%d", user, httpCode), func(t *testing.T) {
+			code := code
+			t.Run(fmt.Sprintf("%s/%d", user, code), func(t *testing.T) {
 				t.Parallel()
 
 				// copy BaseURL and replace auth
@@ -40,7 +52,6 @@ func TestAuth(t *testing.T) {
 				uri := baseURL.ResolveReference(&url.URL{
 					Path: "v1/version",
 				})
-
 				t.Logf("URI: %s", uri)
 				resp, err := http.Get(uri.String())
 				require.NoError(t, err)
@@ -48,7 +59,7 @@ func TestAuth(t *testing.T) {
 
 				b, err := httputil.DumpResponse(resp, true)
 				require.NoError(t, err)
-				assert.Equal(t, httpCode, resp.StatusCode, "response:\n%s", b)
+				assert.Equal(t, code, resp.StatusCode, "response:\n%s", b)
 				require.False(t, bytes.Contains(b, []byte(`<html>`)), "response:\n%s", b)
 			})
 		}
@@ -94,25 +105,28 @@ func TestSetup(t *testing.T) {
 		uri := baseURL.ResolveReference(&url.URL{
 			Path: "/setup",
 		})
+		t.Logf("URI: %s", uri)
 		req, err := http.NewRequest("GET", uri.String(), nil)
 		require.NoError(t, err)
 		req.Header.Set("X-Test-Must-Setup", "1")
 
-		resp, err := client.Do(req)
-		require.NoError(t, err)
-		defer resp.Body.Close() //nolint:errcheck
-		b, err := ioutil.ReadAll(resp.Body)
-		require.NoError(t, err)
+		resp, b := doRequest(t, client, req)
 		assert.Equal(t, 200, resp.StatusCode, "response:\n%s", b)
 		assert.True(t, strings.HasPrefix(string(b), `<!doctype html>`), string(b))
 	})
 
 	t.Run("Redirect", func(t *testing.T) {
 		paths := map[string]int{
-			"graph":      303,
-			"prometheus": 303,
-			"qan":        303,
-			"swagger":    303,
+			"graph":       303,
+			"graph/":      303,
+			"prometheus":  303,
+			"prometheus/": 303,
+			"qan":         303,
+			"qan/":        303,
+
+			// FIXME
+			"swagger":  303,
+			"swagger/": 303,
 
 			"v1/readyz":           200,
 			"v1/AWSInstanceCheck": 405, // only POST is expected
@@ -126,15 +140,12 @@ func TestSetup(t *testing.T) {
 				uri := baseURL.ResolveReference(&url.URL{
 					Path: path,
 				})
+				t.Logf("URI: %s", uri)
 				req, err := http.NewRequest("GET", uri.String(), nil)
 				require.NoError(t, err)
 				req.Header.Set("X-Test-Must-Setup", "1")
 
-				resp, err := client.Do(req)
-				require.NoError(t, err)
-				defer resp.Body.Close() //nolint:errcheck
-				b, err := ioutil.ReadAll(resp.Body)
-				require.NoError(t, err)
+				resp, b := doRequest(t, client, req)
 				assert.Equal(t, code, resp.StatusCode, "response:\n%s", b)
 				if code == 303 {
 					assert.Equal(t, "/setup", resp.Header.Get("Location"))
@@ -149,6 +160,7 @@ func TestSetup(t *testing.T) {
 		uri := baseURL.ResolveReference(&url.URL{
 			Path: "v1/AWSInstanceCheck",
 		})
+		t.Logf("URI: %s", uri)
 		b, err := json.Marshal(server.AWSInstanceCheckBody{
 			InstanceID: "123",
 		})
@@ -157,12 +169,65 @@ func TestSetup(t *testing.T) {
 		require.NoError(t, err)
 		req.Header.Set("X-Test-Must-Setup", "1")
 
-		resp, err := client.Do(req)
-		require.NoError(t, err)
-		defer resp.Body.Close() //nolint:errcheck
-		b, err = ioutil.ReadAll(resp.Body)
-		require.NoError(t, err)
+		resp, b := doRequest(t, client, req)
 		assert.Equal(t, 200, resp.StatusCode, "response:\n%s", b)
 		assert.Equal(t, `{}`, string(b), "response:\n%s", b)
 	})
+}
+
+func TestSwagger(t *testing.T) {
+	// https://jira.percona.com/browse/PMM-5137
+
+	// make client that does not follow redirects
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	for _, path := range []string{
+		"swagger",
+		"swagger/",
+		"swagger.json",
+		"swagger/swagger.json",
+	} {
+		path := path
+
+		t.Run(path, func(t *testing.T) {
+			t.Run("NoAuth", func(t *testing.T) {
+				t.Parallel()
+
+				// make a BaseURL without authentication
+				baseURL, err := url.Parse(pmmapitests.BaseURL.String())
+				require.NoError(t, err)
+				baseURL.User = nil
+
+				uri := baseURL.ResolveReference(&url.URL{
+					Path: path,
+				})
+				t.Logf("URI: %s", uri)
+				req, err := http.NewRequest("GET", uri.String(), nil)
+				require.NoError(t, err)
+
+				resp, _ := doRequest(t, client, req)
+				require.NoError(t, err)
+				assert.Equal(t, 401, resp.StatusCode)
+			})
+
+			t.Run("Auth", func(t *testing.T) {
+				t.Parallel()
+
+				uri := pmmapitests.BaseURL.ResolveReference(&url.URL{
+					Path: path,
+				})
+				t.Logf("URI: %s", uri)
+				req, err := http.NewRequest("GET", uri.String(), nil)
+				require.NoError(t, err)
+
+				resp, _ := doRequest(t, client, req)
+				require.NoError(t, err)
+				assert.Equal(t, 200, resp.StatusCode)
+			})
+		})
+	}
 }
