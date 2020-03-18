fcb267281d61940ba21fb10b68a58869a6822ce9
Carlos Salguero <carlos.salguero@percona.com>
2020-03-18 13:05:06 -0300

    PMM-4192 Added test for MongoDB Explain
    Tested manually with dev-container

--- management/action/explain_test.go
+++ management/action/explain_test.go
@@ -38,3 +38,30 @@ func TestRunExplain(t *testing.T) {
 	require.Empty(t, actionOK.Payload.Error)
 	fmt.Println(actionOK.Payload.Output)
 }
+
+func TestRunMongoDBExplain(t *testing.T) {
+	t.Skip("agent in dev-container is not fully implemented yet")
+
+	explainActionOK, err := client.Default.Actions.StartMongoDBExplainAction(&actions.StartMongoDBExplainActionParams{
+		Context: pmmapitests.Context,
+		Body: actions.StartMongoDBExplainActionBody{
+			ServiceID: "/service_id/f594a0e5-e0c7-4b72-bbca-a931a6279acf",
+			Database:  "test",
+			Query:     "{ quantity: { $gt: 50 } }",
+		},
+	})
+	require.NoError(t, err)
+	require.NotEmpty(t, explainActionOK.Payload.ActionID)
+
+	time.Sleep(2 * time.Second)
+
+	actionOK, err := client.Default.Actions.GetAction(&actions.GetActionParams{
+		Context: pmmapitests.Context,
+		Body: actions.GetActionBody{
+			ActionID: explainActionOK.Payload.ActionID,
+		},
+	})
+	require.NoError(t, err)
+	require.Empty(t, actionOK.Payload.Error)
+	fmt.Println(actionOK.Payload.Output)
+}
