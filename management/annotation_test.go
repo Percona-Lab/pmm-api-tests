654325f033205f752ddd76ed7367a48be4822c8c
Andrii Skomorokhov <andrii.skomorokhov@percona.com>
2020-04-10 09:15:47 +0000

    PMM-5559 Add annotation tests.

---
+++ management/annotation_test.go
@@ -0,0 +1,38 @@
+package management
+
+import (
+	"testing"
+
+	"github.com/percona/pmm/api/managementpb/json/client"
+	"github.com/percona/pmm/api/managementpb/json/client/annotation"
+	"github.com/stretchr/testify/require"
+
+	pmmapitests "github.com/Percona-Lab/pmm-api-tests"
+)
+
+func TestAddAnnotation(t *testing.T) {
+	t.Run("Add Basic Annotation", func(t *testing.T) {
+		params := &annotation.AddAnnotationParams{
+			Body: annotation.AddAnnotationBody{
+				Text: "Annotation Text",
+				Tags: []string{"tag1", "tag2"},
+			},
+			Context: pmmapitests.Context,
+		}
+		resp, err := client.Default.Annotation.AddAnnotation(params)
+		require.NoError(t, err)
+		require.Equal(t, resp.Payload.Message, "Annotation added")
+	})
+
+	t.Run("Add Empty Annotation", func(t *testing.T) {
+		params := &annotation.AddAnnotationParams{
+			Body: annotation.AddAnnotationBody{
+				Text: "",
+				Tags: []string{},
+			},
+			Context: pmmapitests.Context,
+		}
+		_, err := client.Default.Annotation.AddAnnotation(params)
+		require.Error(t, err)
+	})
+}
