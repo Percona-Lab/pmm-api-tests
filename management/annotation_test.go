d14744c9524cae8f6e1ce03bd110ecfdfbed5d42
Jiri Ctvrtka <jiri.ctvrtka@percona.com>
2020-06-22 13:41:23 +0200

    PMM-5563 Required changes.

--- management/annotation_test.go
+++ management/annotation_test.go
@@ -3,14 +3,11 @@ package management
 import (
 	"testing"
 
-	"github.com/AlekSi/pointer"
 	inventoryClient "github.com/percona/pmm/api/inventorypb/json/client"
-	"github.com/percona/pmm/api/inventorypb/json/client/agents"
+	"github.com/percona/pmm/api/inventorypb/json/client/nodes"
 	"github.com/percona/pmm/api/inventorypb/json/client/services"
 	"github.com/percona/pmm/api/managementpb/json/client"
 	"github.com/percona/pmm/api/managementpb/json/client/annotation"
-	mysql "github.com/percona/pmm/api/managementpb/json/client/my_sql"
-	"github.com/percona/pmm/api/managementpb/json/client/node"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc/codes"
@@ -68,94 +65,50 @@ func TestAddAnnotation(t *testing.T) {
 	})
 
 	t.Run("Existing service", func(t *testing.T) {
-		body := node.RegisterNodeBody{
-			NodeName:   "test-node",
-			NodeType:   pointer.ToString(node.RegisterNodeBodyNodeTypeGENERICNODE),
-			Address:    "node-address",
-			Region:     "region",
-			Reregister: true,
-		}
-		paramsRegister := node.RegisterNodeParams{
-			Context: pmmapitests.Context,
-			Body:    body,
-		}
-		node, err := client.Default.Node.RegisterNode(&paramsRegister)
-		require.NoError(t, err)
-
-		nodeID := node.Payload.GenericNode.NodeID
-		pmmAgentID := node.Payload.PMMAgent.AgentID
-
-		defer pmmapitests.RemoveNodes(t, nodeID)
-		defer pmmapitests.RemoveAgents(t, pmmAgentID)
-		nodeExporterAgentID, ok := assertNodeExporterCreated(t, node.Payload.PMMAgent.AgentID)
-		if ok {
-			defer pmmapitests.RemoveAgents(t, nodeExporterAgentID)
-		}
-
-		params := &mysql.AddMySQLParams{
-			Context: pmmapitests.Context,
-			Body: mysql.AddMySQLBody{
-				NodeID:      nodeID,
-				PMMAgentID:  pmmAgentID,
-				ServiceName: "test-service-mysql",
-				Address:     "10.10.10.10",
-				Port:        3306,
-				Username:    "username",
-
-				SkipConnectionCheck: true,
-			},
-		}
-		addMySQLOK, err := client.Default.MySQL.AddMySQL(params)
-		require.NoError(t, err)
-		require.NotNil(t, addMySQLOK)
-		require.NotNil(t, addMySQLOK.Payload.Service)
-		serviceID := addMySQLOK.Payload.Service.ServiceID
-		defer pmmapitests.RemoveServices(t, serviceID)
-
-		// Check that service is created and its fields.
-		serviceOK, err := inventoryClient.Default.Services.GetService(&services.GetServiceParams{
-			Body: services.GetServiceBody{
-				ServiceID: serviceID,
+		nodeName := pmmapitests.TestString(t, "annotation-node")
+		paramsNode := &nodes.AddGenericNodeParams{
+			Body: nodes.AddGenericNodeBody{
+				NodeName: nodeName,
+				Address:  "10.0.0.1",
 			},
 			Context: pmmapitests.Context,
-		})
+		}
+		resNode, err := inventoryClient.Default.Nodes.AddGenericNode(paramsNode)
 		assert.NoError(t, err)
-		require.NotNil(t, serviceOK)
-		assert.Equal(t, services.GetServiceOKBody{
-			Mysql: &services.GetServiceOKBodyMysql{
-				ServiceID:   serviceID,
-				NodeID:      nodeID,
-				ServiceName: "test-service-mysql",
-				Address:     "10.10.10.10",
-				Port:        3306,
-			},
-		}, *serviceOK.Payload)
+		genericNodeID := resNode.Payload.Generic.NodeID
+		defer pmmapitests.RemoveNodes(t, genericNodeID)
 
-		// Check that mysqld exporter is added by default.
-		listAgents, err := inventoryClient.Default.Agents.ListAgents(&agents.ListAgentsParams{
-			Context: pmmapitests.Context,
-			Body: agents.ListAgentsBody{
-				ServiceID: serviceID,
+		serviceName := pmmapitests.TestString(t, "annotation-service")
+		paramsService := &services.AddMySQLServiceParams{
+			Body: services.AddMySQLServiceBody{
+				NodeID:      genericNodeID,
+				Address:     "localhost",
+				Port:        3306,
+				ServiceName: serviceName,
 			},
-		})
+			Context: pmmapitests.Context,
+		}
+		resService, err := inventoryClient.Default.Services.AddMySQLService(paramsService)
 		assert.NoError(t, err)
-		assert.Equal(t, agents.ListAgentsOKBody{
-			MysqldExporter: []*agents.MysqldExporterItems0{
-				{
-					AgentID:                   listAgents.Payload.MysqldExporter[0].AgentID,
-					ServiceID:                 serviceID,
-					PMMAgentID:                pmmAgentID,
-					Username:                  "username",
-					TablestatsGroupTableLimit: 1000,
+		require.NotNil(t, resService)
+		serviceID := resService.Payload.Mysql.ServiceID
+		assert.Equal(t, &services.AddMySQLServiceOK{
+			Payload: &services.AddMySQLServiceOKBody{
+				Mysql: &services.AddMySQLServiceOKBodyMysql{
+					ServiceID:   serviceID,
+					NodeID:      genericNodeID,
+					Address:     "localhost",
+					Port:        3306,
+					ServiceName: serviceName,
 				},
 			},
-		}, *listAgents.Payload)
-		defer removeAllAgentsInList(t, listAgents)
+		}, resService)
+		defer pmmapitests.RemoveServices(t, serviceID)
 
 		paramsAdd := &annotation.AddAnnotationParams{
 			Body: annotation.AddAnnotationBody{
 				Text:         "Some text",
-				ServiceNames: []string{"test-service-mysql"},
+				ServiceNames: []string{serviceName},
 			},
 			Context: pmmapitests.Context,
 		}
@@ -164,31 +117,22 @@ func TestAddAnnotation(t *testing.T) {
 	})
 
 	t.Run("Existing node", func(t *testing.T) {
-		body := node.RegisterNodeBody{
-			NodeName:   "test-node",
-			NodeType:   pointer.ToString(node.RegisterNodeBodyNodeTypeGENERICNODE),
-			Address:    "node-address",
-			Region:     "region",
-			Reregister: true,
-		}
-		paramsRegister := node.RegisterNodeParams{
+		nodeName := pmmapitests.TestString(t, "annotation-node")
+		params := &nodes.AddGenericNodeParams{
+			Body: nodes.AddGenericNodeBody{
+				NodeName: nodeName,
+				Address:  "10.0.0.1",
+			},
 			Context: pmmapitests.Context,
-			Body:    body,
 		}
-		node, err := client.Default.Node.RegisterNode(&paramsRegister)
-		require.NoError(t, err)
-
-		defer pmmapitests.RemoveNodes(t, node.Payload.GenericNode.NodeID)
-		defer pmmapitests.RemoveAgents(t, node.Payload.PMMAgent.AgentID)
-		nodeExporterAgentID, ok := assertNodeExporterCreated(t, node.Payload.PMMAgent.AgentID)
-		if ok {
-			defer pmmapitests.RemoveAgents(t, nodeExporterAgentID)
-		}
+		res, err := inventoryClient.Default.Nodes.AddGenericNode(params)
+		assert.NoError(t, err)
+		defer pmmapitests.RemoveNodes(t, res.Payload.Generic.NodeID)
 
 		paramsAdd := &annotation.AddAnnotationParams{
 			Body: annotation.AddAnnotationBody{
 				Text:     "Some text",
-				NodeName: "test-node",
+				NodeName: nodeName,
 			},
 			Context: pmmapitests.Context,
 		}
