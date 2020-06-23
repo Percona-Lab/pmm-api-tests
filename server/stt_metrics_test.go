4d8fff08082e775144d5502a997a04a9bdc0e46d
Palash Nigam <palash.nigam@percona.com>
2020-06-23 18:18:53 +0530

    SAAS-104: Add check for metric values

--- server/stt_metrics_test.go
+++ server/stt_metrics_test.go
@@ -2,12 +2,11 @@ package server
 
 import (
 	"context"
+	"net/url"
+	"strings"
 	"testing"
 	"time"
 
-	managementClient "github.com/percona/pmm/api/managementpb/json/client"
-	serverClient "github.com/percona/pmm/api/serverpb/json/client"
-	"github.com/percona/pmm/api/serverpb/json/client/server"
 	"github.com/prometheus/client_golang/api"
 	promapi "github.com/prometheus/client_golang/api/prometheus/v1"
 	"github.com/stretchr/testify/assert"
@@ -16,55 +15,89 @@ import (
 	pmmapitests "github.com/Percona-Lab/pmm-api-tests"
 )
 
-const promAddress = "http://admin:admin@127.0.0.1:80/prometheus"
-
 func TestSTTMetrics(t *testing.T) {
-	client := serverClient.Default.Server
-
 	t.Run("StartSTTChecksAndRecordMetrics", func(t *testing.T) {
-		defer restoreSettingsDefaults(t)
-		// Enabled STT
-		res, err := client.ChangeSettings(&server.ChangeSettingsParams{
-			Body: server.ChangeSettingsBody{
-				EnableStt:       true,
-				EnableTelemetry: true,
-			},
-			Context: pmmapitests.Context,
-		})
-		require.NoError(t, err)
-		assert.True(t, res.Payload.Settings.SttEnabled)
-		assert.True(t, res.Payload.Settings.TelemetryEnabled)
-		assert.Empty(t, err)
-
-		resp, err := managementClient.Default.SecurityChecks.StartSecurityChecks(nil)
-		require.NoError(t, err)
-		assert.NotNil(t, resp)
-
 		client, err := api.NewClient(api.Config{
-			Address: promAddress,
+			Address: pmmapitests.BaseURL.ResolveReference(&url.URL{
+				Path: "/prometheus",
+			}).String(),
 		})
 		require.NoError(t, err)
 		promClient := promapi.NewAPI(client)
 
 		testCases := []struct {
-			query string
-			len   int
+			query          string
+			len            int
+			metricType     string
+			expectedValues []string
 		}{
 			{
-				query: "pmm_managed_checks_alerts_generated_total",
-				len:   7,
+				query:      "pmm_managed_checks_alerts_generated_total",
+				len:        7,
+				metricType: "vector",
+				expectedValues: []string{
+					`pmm_managed_checks_alerts_generated_total
+					{check_type="MONGODB_BUILDINFO", 
+					instance="pmm-server", job="pmm-managed", 
+					service_type="mongodb"} => 0`,
+					`pmm_managed_checks_alerts_generated_total
+					{check_type="MONGODB_GETCMDLINEOPTS", 
+					instance="pmm-server", job="pmm-managed", 
+					service_type="mongodb"} => 0`,
+					`pmm_managed_checks_alerts_generated_total
+					{check_type="MONGODB_GETPARAMETER", 
+					instance="pmm-server", job="pmm-managed", 
+					service_type="mongodb"} => 0`,
+					`pmm_managed_checks_alerts_generated_total
+					{check_type="MYSQL_SELECT", 
+					instance="pmm-server", job="pmm-managed", 
+					service_type="mysql"} => 0`,
+					`pmm_managed_checks_alerts_generated_total
+					{check_type="MYSQL_SHOW", 
+					instance="pmm-server", job="pmm-managed", 
+					service_type="mysql"} => 0`,
+					`pmm_managed_checks_alerts_generated_total
+					{check_type="POSTGRESQL_SELECT", 
+					instance="pmm-server", job="pmm-managed", 
+					service_type="postgresql"} => 0`,
+					`pmm_managed_checks_alerts_generated_total
+					{check_type="POSTGRESQL_SHOW", 
+					instance="pmm-server", job="pmm-managed", 
+					service_type="postgresql"} => 0`},
 			},
 			{
-				query: "pmm_managed_checks_scripts_executed_total",
-				len:   3,
+				query:      "pmm_managed_checks_scripts_executed_total",
+				len:        3,
+				metricType: "vector",
+				expectedValues: []string{`
+				pmm_managed_checks_scripts_executed_total
+				{instance="pmm-server", job="pmm-managed", 
+				service_type="mongodb"} => 0`,
+					`pmm_managed_checks_scripts_executed_total
+				{instance="pmm-server", job="pmm-managed", 
+				service_type="mysql"} => 0`,
+					`pmm_managed_checks_scripts_executed_total
+				{instance="pmm-server", job="pmm-managed", 
+				service_type="postgresql"} => 0`},
 			},
 		}
 
 		for _, tc := range testCases {
-			result, _, err := promClient.Query(context.Background(), tc.query, time.Now())
+			result, _, err := promClient.Query(context.Background(),
+				tc.query, time.Now())
+
+			var actualValues []string
+			for _, s := range strings.Split(result.String(), "\n") {
+				// remove the timestamp from the values
+				metric := strings.Split(s, " @")
+				actualValues = append(actualValues, metric[0])
+			}
+
 			require.NoError(t, err)
 			assert.NotEmpty(t, result)
 			assert.Len(t, result, tc.len)
+			assert.Equal(t, tc.metricType, result.Type().String())
+			assert.Equal(t, tc.expectedValues, actualValues)
 		}
 	})
 }
