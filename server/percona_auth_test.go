07105e03f7ffe13296557bb59a11989bd697b34c
Artem Gavrilov <artem.gavrilov@percona.com>
2020-07-16 17:39:54 +0200

    SAAS-175 Add auth API tests

---
+++ server/percona_auth_test.go
@@ -0,0 +1,36 @@
+package server
+
+import (
+	"testing"
+
+	"github.com/brianvoe/gofakeit"
+	serverClient "github.com/percona/pmm/api/serverpb/json/client"
+	"github.com/percona/pmm/api/serverpb/json/client/server"
+	"github.com/stretchr/testify/require"
+
+	pmmapitests "github.com/Percona-Lab/pmm-api-tests"
+)
+
+func TestPerconaAuth(t *testing.T) {
+	client := serverClient.Default.Server
+	login := gofakeit.Email()
+	password := "Password12345"
+
+	_, err := client.SignUp(&server.SignUpParams{
+		Body: server.SignUpBody{
+			Email:    login,
+			Password: password,
+		},
+		Context: pmmapitests.Context,
+	})
+	require.NoError(t, err)
+
+	_, err = client.SignIn(&server.SignInParams{
+		Body: server.SignInBody{
+			Email:    login,
+			Password: password,
+		},
+		Context: pmmapitests.Context,
+	})
+	require.NoError(t, err)
+}
