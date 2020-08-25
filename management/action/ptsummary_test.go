10bbb66878573c505a7a21e5b1ed96ac67b2c30b
Jiří Čtvrtka <jiri.ctvrtka.sde@gmail.com>
2020-08-25 15:52:17 +0200

    PMM-6038 Lint.

--- management/action/ptsummary_test.go
+++ management/action/ptsummary_test.go
@@ -7,28 +7,8 @@ import (
 
 	"github.com/percona/pmm/api/managementpb/json/client"
 	"github.com/percona/pmm/api/managementpb/json/client/actions"
-	"github.com/percona/pmm/api/managementpb/json/client/node"
 	"github.com/stretchr/testify/require"
-
-	pmmapitests "github.com/Percona-Lab/pmm-api-tests"
 )
-
-func registerGenericNode(t pmmapitests.TestingT, body node.RegisterNodeBody) (string, string) {
-	t.Helper()
-
-	params := node.RegisterNodeParams{
-		Context: pmmapitests.Context,
-		Body:    body,
-	}
-	registerOK, err := client.Default.Node.RegisterNode(&params)
-	require.NoError(t, err)
-	require.NotNil(t, registerOK)
-	require.NotNil(t, registerOK.Payload.PMMAgent)
-	require.NotNil(t, registerOK.Payload.PMMAgent.AgentID)
-	require.NotNil(t, registerOK.Payload.GenericNode)
-	require.NotNil(t, registerOK.Payload.GenericNode.NodeID)
-	return registerOK.Payload.GenericNode.NodeID, registerOK.Payload.PMMAgent.AgentID
-}
 
 func TestPTSummary(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
@@ -54,6 +34,7 @@ func TestPTSummary(t *testing.T) {
 
 		if !actionOK.Payload.Done {
 			time.Sleep(1 * time.Second)
+
 			continue
 		}
 
