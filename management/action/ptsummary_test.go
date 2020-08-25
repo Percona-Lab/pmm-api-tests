0d420a8b683a5ad6bf898c529b71f9f224e2230c
Jiří Čtvrtka <jiri.ctvrtka.sde@gmail.com>
2020-08-25 14:42:25 +0200

    PMM-6038 Remove optional param.

--- management/action/ptsummary_test.go
+++ management/action/ptsummary_test.go
@@ -17,8 +17,7 @@ func TestPTSummary(t *testing.T) {
 	explainActionOK, err := client.Default.Actions.StartPTSummaryAction(&actions.StartPTSummaryActionParams{
 		Context: pmmapitests.Context,
 		Body: actions.StartPTSummaryActionBody{
-			PMMAgentID: "/agent_id/12856e6b-5f4a-49cb-9c70-54edd0e0a074",
-			NodeID:     "/node_id/5a9a7aa6-7af4-47be-817c-6d88e955bff2",
+			NodeID: "/node_id/5a9a7aa6-7af4-47be-817c-6d88e955bff2",
 		},
 	})
 	require.NoError(t, err)
