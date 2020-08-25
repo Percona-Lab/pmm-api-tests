29c5cf9d832727e21d13a62c5eb261bcf7c55861
Jiří Čtvrtka <jiri.ctvrtka.sde@gmail.com>
2020-08-25 15:30:41 +0200

    PMM-6038 Remove skip for test, add timeout.

--- management/action/ptsummary_test.go
+++ management/action/ptsummary_test.go
@@ -1,32 +1,51 @@
 package action
 
 import (
+	"context"
 	"testing"
 	"time"
 
 	"github.com/percona/pmm/api/managementpb/json/client"
 	"github.com/percona/pmm/api/managementpb/json/client/actions"
+	"github.com/percona/pmm/api/managementpb/json/client/node"
 	"github.com/stretchr/testify/require"
 
 	pmmapitests "github.com/Percona-Lab/pmm-api-tests"
 )
 
+func registerGenericNode(t pmmapitests.TestingT, body node.RegisterNodeBody) (string, string) {
+	t.Helper()
+
+	params := node.RegisterNodeParams{
+		Context: pmmapitests.Context,
+		Body:    body,
+	}
+	registerOK, err := client.Default.Node.RegisterNode(&params)
+	require.NoError(t, err)
+	require.NotNil(t, registerOK)
+	require.NotNil(t, registerOK.Payload.PMMAgent)
+	require.NotNil(t, registerOK.Payload.PMMAgent.AgentID)
+	require.NotNil(t, registerOK.Payload.GenericNode)
+	require.NotNil(t, registerOK.Payload.GenericNode.NodeID)
+	return registerOK.Payload.GenericNode.NodeID, registerOK.Payload.PMMAgent.AgentID
+}
+
 func TestPTSummary(t *testing.T) {
-	t.Skip("not implemented yet")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
 
 	explainActionOK, err := client.Default.Actions.StartPTSummaryAction(&actions.StartPTSummaryActionParams{
-		Context: pmmapitests.Context,
+		Context: ctx,
 		Body: actions.StartPTSummaryActionBody{
-			NodeID: "/node_id/5a9a7aa6-7af4-47be-817c-6d88e955bff2",
+			NodeID: "pmm-server",
 		},
 	})
 	require.NoError(t, err)
 	require.NotEmpty(t, explainActionOK.Payload.ActionID)
 
-	var retries int
 	for {
 		actionOK, err := client.Default.Actions.GetAction(&actions.GetActionParams{
-			Context: pmmapitests.Context,
+			Context: ctx,
 			Body: actions.GetActionBody{
 				ActionID: explainActionOK.Payload.ActionID,
 			},
@@ -35,10 +54,7 @@ func TestPTSummary(t *testing.T) {
 
 		if !actionOK.Payload.Done {
 			time.Sleep(1 * time.Second)
-			retries++
-			if retries < 5 {
-				continue
-			}
+			continue
 		}
 
 		require.True(t, actionOK.Payload.Done)
