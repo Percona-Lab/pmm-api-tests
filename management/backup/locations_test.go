a5171375469d33b999f87ca335de8f4dd26aebb2
Dasio <david.mikus@sde.cz>
2021-02-03 14:52:50 +0100

    PMM-7380 List BackupLocation test

--- management/backup/locations_test.go
+++ management/backup/locations_test.go
@@ -3,6 +3,8 @@ package backup
 import (
 	"testing"
 
+	"google.golang.org/grpc/codes"
+
 	"github.com/brianvoe/gofakeit"
 	backupClient "github.com/percona/pmm/api/managementpb/backup/json/client"
 	"github.com/percona/pmm/api/managementpb/backup/json/client/locations"
@@ -32,6 +34,115 @@ func TestAddLocation(t *testing.T) {
 		assert.NotEmpty(t, resp.Payload.LocationID)
 	})
 
+	t.Run("normal s3 config", func(t *testing.T) {
+		resp, err := client.AddLocation(&locations.AddLocationParams{
+			Body: locations.AddLocationBody{
+				Name:        gofakeit.Name(),
+				Description: gofakeit.Question(),
+				S3Config: &locations.AddLocationParamsBodyS3Config{
+					Endpoint:  "http://example.com",
+					AccessKey: "access_key",
+					SecretKey: "secret_key",
+				},
+			},
+			Context: pmmapitests.Context,
+		})
+		require.NoError(t, err)
+		defer deleteLocation(t, client, resp.Payload.LocationID)
+
+		assert.NotEmpty(t, resp.Payload.LocationID)
+	})
+
+	t.Run("missing name", func(t *testing.T) {
+		resp, err := client.AddLocation(&locations.AddLocationParams{
+			Body: locations.AddLocationBody{
+				Name:        gofakeit.Name(),
+				Description: gofakeit.Question(),
+			},
+			Context: pmmapitests.Context,
+		})
+
+		pmmapitests.AssertAPIErrorf(t, err, 400, codes.InvalidArgument, "Missing location type")
+		assert.Nil(t, resp)
+	})
+
+	t.Run("missing config", func(t *testing.T) {
+		resp, err := client.AddLocation(&locations.AddLocationParams{
+			Body: locations.AddLocationBody{
+				Name:        gofakeit.Name(),
+				Description: gofakeit.Question(),
+			},
+			Context: pmmapitests.Context,
+		})
+
+		pmmapitests.AssertAPIErrorf(t, err, 400, codes.InvalidArgument, "Missing location type")
+		assert.Nil(t, resp)
+	})
+
+	t.Run("missing fs path", func(t *testing.T) {
+		resp, err := client.AddLocation(&locations.AddLocationParams{
+			Body: locations.AddLocationBody{
+				Name:        gofakeit.Name(),
+				Description: gofakeit.Question(),
+				FsConfig:    &locations.AddLocationParamsBodyFsConfig{},
+			},
+			Context: pmmapitests.Context,
+		})
+
+		pmmapitests.AssertAPIErrorf(t, err, 400, codes.InvalidArgument, "invalid field FsConfig.Path: value '' must not be an empty string")
+		assert.Nil(t, resp)
+	})
+
+	t.Run("missing s3 endpoint", func(t *testing.T) {
+		resp, err := client.AddLocation(&locations.AddLocationParams{
+			Body: locations.AddLocationBody{
+				Name:        gofakeit.Name(),
+				Description: gofakeit.Question(),
+				S3Config: &locations.AddLocationParamsBodyS3Config{
+					Endpoint:  "",
+					AccessKey: "access_key",
+					SecretKey: "secret_key",
+				},
+			},
+			Context: pmmapitests.Context,
+		})
+
+		pmmapitests.AssertAPIErrorf(t, err, 400, codes.InvalidArgument, "invalid field FsConfig.Endpoint: value '' must not be an empty string")
+		assert.Nil(t, resp)
+	})
+}
+
+func TestListLocations(t *testing.T) {
+	client := backupClient.Default.Locations
+
+	body := locations.AddLocationBody{
+		Name:        gofakeit.Name(),
+		Description: gofakeit.Question(),
+		FsConfig: &locations.AddLocationParamsBodyFsConfig{
+			Path: "/tmp",
+		},
+	}
+	addResp, err := client.AddLocation(&locations.AddLocationParams{
+		Body:    body,
+		Context: pmmapitests.Context,
+	})
+	require.NoError(t, err)
+	defer deleteLocation(t, client, addResp.Payload.LocationID)
+
+	resp, err := client.ListLocations(&locations.ListLocationsParams{Context: pmmapitests.Context})
+	require.NoError(t, err)
+
+	assert.NotEmpty(t, resp.Payload.Locations)
+	var found bool
+	for _, loc := range resp.Payload.Locations {
+		if loc.LocationID == addResp.Payload.LocationID {
+			assert.Equal(t, body.Name, loc.Name)
+			assert.Equal(t, body.Description, loc.Description)
+			assert.Equal(t, body.FsConfig.Path, loc.FsConfig.Path)
+			found = true
+		}
+	}
+	assert.True(t, found, "Expected location not found")
 }
 
 func deleteLocation(t *testing.T, client locations.ClientService, id string) {
