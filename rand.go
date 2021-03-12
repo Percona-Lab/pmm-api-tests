cda2896daf1d5c431590d895c5be9bf98852156b
Dasio <david.mikus@sde.cz>
2021-03-12 12:40:25 +0100

    PMM-7626 Comments

--- rand.go
+++ rand.go
@@ -1,3 +1,4 @@
+// This file contains implementation for concurrent safe RNG.
 package pmmapitests
 
 import (
@@ -5,29 +6,35 @@ import (
 	"sync"
 )
 
+// ConcurrentRand wraps rand.Rand with mutex.
 type ConcurrentRand struct {
 	m    sync.Mutex
 	rand *rand.Rand
 }
 
+// NewConcurrentRand constructs new ConcurrentRand with provided seed.
 func NewConcurrentRand(seed int64) *ConcurrentRand {
 	r := &ConcurrentRand{
 		rand: rand.New(rand.NewSource(seed)),
 	}
 	return r
 }
-func (r *ConcurrentRand) Int63() int64 {
-	r.m.Lock()
-	defer r.m.Unlock()
-	return r.rand.Int63()
-}
 
+// Seed uses the provided seed value to initialize the generator to a deterministic state.
 func (r *ConcurrentRand) Seed(seed int64) {
 	r.m.Lock()
 	defer r.m.Unlock()
 	r.rand.Seed(seed)
 }
 
+// Int63 returns a non-negative pseudo-random 63-bit integer as an int64.
+func (r *ConcurrentRand) Int63() int64 {
+	r.m.Lock()
+	defer r.m.Unlock()
+	return r.rand.Int63()
+}
+
+// Uint64 returns a pseudo-random 64-bit value as a uint64.
 func (r *ConcurrentRand) Uint64() uint64 {
 	r.m.Lock()
 	defer r.m.Unlock()
